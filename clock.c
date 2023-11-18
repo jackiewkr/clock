e807d1524a111fd5527debf4ab0331bcbe2bdffa
Jacqueline <jackiewalkergeneral@gmail.com>
2023-11-18 17:05:09 +0000

    Aligned with current coding style
    Separated ASCII art from being hard-coded by including from a separate file.
    Rewrote parts of the code to better fit in with my current coding style and
    to clean up sections of sloppy coding.

--- clock.c
+++ clock.c
@@ -1,173 +1,140 @@
-/*
-Clock app made to learn C and ncurses.
+/* clock.c
+ * =======
+ * Simple program that displays the current time at the center of the screen
+ * using curses. Press Q to exit cleanly.
+ *
+ * By Jacqueline W.
+ */
 
-By Jackie W.
-
-21 dec 21
-*/
+#include "ascii_chars.h"
 
 #include <ncurses.h>
-#include <stdbool.h>
 #include <time.h>
+#include <string.h>
 
-void enterCurses() {
-  initscr();
-  keypad(stdscr, TRUE);
-  halfdelay(10);
-  //nodelay(stdscr,true);
-  noecho();
-  curs_set(0);
+/* enter_ncurses()
+ * ==============
+ * Enters curses mode and sets some input options.
+ */
+void enter_ncurses( void )
+{
+        initscr();
+        halfdelay( 10 );                //blocks getch() for 1 second
+        noecho();                       //don't echo keys pressed to the term 
+        curs_set( 0 );                  //hide cursor
 }
 
-void exitCurses() {
-  nocbreak();
-  echo();
-  curs_set(1);
-  endwin();
+/* exit_ncurses()
+ * =============
+ * Resets options changed and exits curses mode.
+ */
+void exit_ncurses ( void )
+{
+        nocbreak();
+        echo();
+        curs_set(1);
+        endwin();
 }
 
-void printBigColon(int row, int col,int sec){
-  int startCol = (col-17)/2;
-  int startRow = (row-5)/2;
-  int colOffset = 4*2; //rough column offset
-  if (sec % 2 == 0){
-    mvprintw(startRow+1,startCol+colOffset+1,"#");
-    mvprintw(startRow+3,startCol+colOffset+1,"#");
-  }
+/* print_ascii_char()
+ * ==================
+ * Prints a character to the terminal screen at the given row and column
+ * starting location.
+ * Only prints characters from list [0,1,2,3,4,5,6,7,8,9,:].
+ */
+void print_ascii_char( char ch, int col, int row )
+{
+        char valid_chars[11] = "0123456789:";
+
+        if ( strchr( valid_chars, ch ) != NULL )
+        {
+                char ascii_char[CHAR_HEIGHT][CHAR_WIDTH] = {};
+
+                switch ( ch )
+                {
+                case '0':
+                        memcpy( ascii_char, ascii_0, sizeof(ascii_char) );
+                        break;
+                case '1':
+                        memcpy( ascii_char, ascii_1, sizeof(ascii_char) );
+                        break;
+                case '2':
+                        memcpy( ascii_char, ascii_2, sizeof(ascii_char) );
+                        break;
+                case '3':
+                        memcpy( ascii_char, ascii_3, sizeof(ascii_char) );
+                        break;
+                case '4':
+                        memcpy( ascii_char, ascii_4, sizeof(ascii_char) );
+                        break;
+                case '5':
+                        memcpy( ascii_char, ascii_5, sizeof(ascii_char) );
+                        break;
+                case '6':
+                        memcpy( ascii_char, ascii_6, sizeof(ascii_char) );
+                        break;
+                case '7':
+                        memcpy( ascii_char, ascii_7, sizeof(ascii_char) );
+                        break;
+                case '8':
+                        memcpy( ascii_char, ascii_8, sizeof(ascii_char) );
+                        break;
+                case '9':
+                        memcpy( ascii_char, ascii_9, sizeof(ascii_char) );
+                        break;
+                case ':':
+                        memcpy( ascii_char, ascii_colon, sizeof(ascii_char) );
+                        break;
+                }
+
+                for ( int i = 0; i < CHAR_HEIGHT; i++ )
+                {
+                        mvprintw( row + i, col, "%.*s", CHAR_WIDTH, 
+                                  ascii_char[i] );
+                }
+        }
 }
 
-void printBigNum(int num, int pos,int row, int col) {
-  /*Prints big ASCII art numbers at a given place
-   int num: number to be displayed
-   int pos: place for number to be positioned (0-3)*/
-  int startCol = (col-17)/2;
-  int startRow = (row-5)/2;
-  int colOffset = 4*pos; //rough column offset
-  if (pos > 1) {
-    colOffset = colOffset + 3;
-  }
-  switch (num) {
-  case 0:
-    mvprintw(startRow,startCol+colOffset,"###");
-    mvprintw(startRow+1,startCol+colOffset,"# #");
-    mvprintw(startRow+2,startCol+colOffset,"# #");
-    mvprintw(startRow+3,startCol+colOffset,"# #");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 1:
-    mvprintw(startRow,startCol+colOffset," # ");
-    mvprintw(startRow+1,startCol+colOffset,"## ");
-    mvprintw(startRow+2,startCol+colOffset," # ");
-    mvprintw(startRow+3,startCol+colOffset," #");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 2:
-    mvprintw(startRow,startCol+colOffset,"###");
-    mvprintw(startRow+1,startCol+colOffset,"  #");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"# ");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 3:
-    mvprintw(startRow,startCol+colOffset,  "###");
-    mvprintw(startRow+1,startCol+colOffset,"  #");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"  #");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 4:
-    mvprintw(startRow,startCol+colOffset,  "# #");
-    mvprintw(startRow+1,startCol+colOffset,"# #");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"  #");
-    mvprintw(startRow+4,startCol+colOffset,"  #");
-    break;
-  case 5:
-    mvprintw(startRow,startCol+colOffset,  "###");
-    mvprintw(startRow+1,startCol+colOffset,"# ");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"  #");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 6:
-    mvprintw(startRow,startCol+colOffset,  "###");
-    mvprintw(startRow+1,startCol+colOffset,"# ");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"# #");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 7:
-    mvprintw(startRow,startCol+colOffset,  "###");
-    mvprintw(startRow+1,startCol+colOffset,"  #");
-    mvprintw(startRow+2,startCol+colOffset,"  #");
-    mvprintw(startRow+3,startCol+colOffset,"  #");
-    mvprintw(startRow+4,startCol+colOffset,"  #");
-    break;
-  case 8:
-    mvprintw(startRow,startCol+colOffset,  "###");
-    mvprintw(startRow+1,startCol+colOffset,"# #");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"# #");
-    mvprintw(startRow+4,startCol+colOffset,"###");
-    break;
-  case 9:
-    mvprintw(startRow,startCol+colOffset,  "###");
-    mvprintw(startRow+1,startCol+colOffset,"# #");
-    mvprintw(startRow+2,startCol+colOffset,"###");
-    mvprintw(startRow+3,startCol+colOffset,"  #");
-    mvprintw(startRow+4,startCol+colOffset,"  #");
-    break;
-  default:
-    printw("%d",num);
-  }
-}
+int main( void )
+{
+        enter_ncurses();
 
-int main(){
-  enterCurses();
+        int row, col;
+        unsigned int should_close = 0;
+        while ( !should_close )
+        {
+                // get current time
+                time_t raw_time;
+                struct tm* time_struct;
 
-  //bool for whether the program should close
-  bool loop;
-  loop = true;
+                time ( &raw_time );
+                time_struct = localtime( &raw_time );
 
-  int row, col;
+                // get terminal dims
+                getmaxyx( stdscr, row, col );
 
-  while (loop == 1) {
-    //main loop of the program
+                // convert time to string
+                char buf[6] = "";
+                snprintf( buf, 6, "%02d:%02d", time_struct->tm_hour, 
+                          time_struct->tm_min );
 
-    //get current time
-    time_t rawCurrTime;
-    struct tm * currTime;
+                // print string
+                for ( int i = 0; i < 5; i++ )
+                {
+                        int curr_row = (row - CHAR_HEIGHT)/2;
+                        int curr_col = ( col - CHAR_WIDTH * 5 )/2 + 
+                                       i * (CHAR_WIDTH + CHAR_PADDING);
+                        print_ascii_char( buf[i], curr_col, curr_row );
+                }
 
-    time(&rawCurrTime);
-    currTime = localtime(&rawCurrTime);
-
-    //get new screen dims
-    getmaxyx(stdscr,row,col);
-
-    //calc numbers to be displayed for each pos
-    int msb_hour = 0;
-    if (currTime->tm_hour > 9) {
-      msb_hour = currTime->tm_hour / 10;
-    } 
-    int lsb_hour = currTime->tm_hour - (msb_hour * 10);
-
-    int msb_min = 0;
-    if (currTime->tm_min > 9) {
-      msb_min = currTime->tm_min / 10;
-    }
-    int lsb_min = currTime->tm_min - msb_min *10;
-    printBigNum(msb_hour,0,row,col);
-    printBigNum(lsb_hour,1,row,col);
-    printBigNum(msb_min,2,row,col);
-    printBigNum(lsb_min,3,row,col);
-    printBigColon(row,col,currTime->tm_sec);
-
-    //refresh the screen
-    refresh();
-    getch();
-    erase();
-  }
-  exitCurses();
-  return 0;
+                refresh();
+                if( (char)getch() == 'q' )
+                {
+                        should_close++;
+                }
+                erase();
+        }
+        exit_ncurses();
+        return 0;
 }
 
